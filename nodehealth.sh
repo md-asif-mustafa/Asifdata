96c7815122d0ad9aaaeb194e2f46b5d398908b07
md-asif-mustafa <mdasif.april@gmail.com>
2024-07-14 22:00:45 +0530

    new shell script

--- nodehealth.sh
+++ nodehealth.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 #######################
-# Author
+# Author: Asif
+# Date: 14/07/2024
+
+# This script output the node health
+
+# Version: v1
+#######################
+
+df -h
+
+free -g
+
+nproc
