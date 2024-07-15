2f6320facead5dc63627fc3b79ba68e0bff10828
md-asif-mustafa <mdasif.april@gmail.com>
2024-07-15 19:06:46 +0530

    shell

--- nodehealth.sh
+++ nodehealth.sh
@@ -7,13 +7,12 @@
 # This script output the node health
 
 # Version: v1
-########################
+#########################
 
 set -x # debug mode
 set -e # exit the script when there is error
 set -o pipefail
 
-jgjkjg | echo 
 
 df -h
 
